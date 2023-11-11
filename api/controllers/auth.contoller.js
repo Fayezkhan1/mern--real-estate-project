9def46e6ab13ea660bbf19e6a6878ba196d1837f
Fayez <fayezkhan680@gmail.com>
2023-11-11 12:00:44 +0530

    complete sign up page functionality

--- api/controllers/auth.contoller.js
+++ api/controllers/auth.contoller.js
@@ -9,7 +9,7 @@ export const signup =async (req,res,next)=>{
     
     try{
         await newUser.save()
-    res.status(201).json("User Create Successfully")
+    res.status(201).json({ success: true, message: 'User Created Successfully' })
     } catch(error){
         next(error);
     }
